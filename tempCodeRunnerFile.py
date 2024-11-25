7a69007409ca951d33b5226a1ec1c161011ea766
Shrestha Shome Ullas <shrestha.shome@gmail.com>
2024-11-26 03:05:14 +0600

    4

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-chemistry = pybamm.parameter_sets.Chen2020
-print(chemistry)
-params = pybamm.ParameterValues(chemistry=chemistry)
-print(params)
-params.search("electrolyte")
-model.print_parameter_info()
-sim = pybamm.Simulation(model, parameter_values=params)
-sim.solve([0, 3600])
